856dc23bc21dec8b59feb272e98dd199cd5caa10
https://github.com/arpit10saluja/git_we.git <arpitsaluja1234@gmail.com>
2023-01-22 15:07:23 +0530

    day-4

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Button, Grid } from "@chakra-ui/react"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import Product from "./Products"
 
-function Home() {
+
+export default function Home() {
+
+  const [ProductData, setProductData]=useState([])
+
+  useEffect(() => {
+    axios.get(`http://localhost:${8080}/products`)
+    .then((res)=>setProductData(res?.data))
+    .catch((err)=>console.log(err))  
+    console.log(ProductData)
+
+  },[])
+  
+
+
   return (
-    <div>Home</div>
+    <>
+
+    <Button>General Products</Button>
+    <Button>Male Products</Button>
+    <Button>Female Products</Button>
+
+    <Grid templateColumns='repeat(4, 1fr)' gap={"20px"} margin="50px">
+        {ProductData.map((product)=>(
+        <Product props={product}/>
+        ))}
+    </Grid>
+    </>
+
   )
 }
 
-export default Home
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import {Card, CardBody, Image, Stack, Heading, Text, Divider, CardFooter, ButtonGroup, Button} from "@chakra-ui/react"
+function Product({props}) {
+  const {id, brand, description, images, name, price, ratings, strikeOfPrice, type}= props
 
-function Product() {
   return (
-    <div>products</div>
+    <div>
+      <Card maxW='sm' key={id} width={"auto"}>
+  <CardBody>
+    <Image
+      src={images}
+      alt={props.name}
+      borderRadius='lg'
+      height={"50%"}
+    />
+    <Stack mt='6' spacing='3'>
+      <Heading size='md'>{name}</Heading>
+      <Text>
+        {description}
+      </Text>
+      <Text color='blue.600' fontSize='xl' as='del'>
+        ${strikeOfPrice}
+      </Text>
+      <Text color='blue.600' fontSize='2xl'>
+        ${price}
+      </Text>
+
+    </Stack>
+  </CardBody>
+  <Divider />
+  <CardFooter>
+    <ButtonGroup spacing='2'>
+      <Button variant='solid' colorScheme='blue'>
+        Buy now
+      </Button>
+      <Button variant='ghost' colorScheme='blue'>
+        Add to cart
+      </Button>
+    </ButtonGroup>
+  </CardFooter>
+</Card>
+
+    </div>
   )
 }
 
