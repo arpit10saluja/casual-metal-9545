c1cab0c7dfdf0a5d39d4b63bb3286c82542f6bc2
https://github.com/arpit10saluja/git_we.git <arpitsaluja1234@gmail.com>
2023-01-22 18:13:33 +0530

    day-4

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,36 +1,59 @@
-import { Button, Grid } from "@chakra-ui/react"
+import { Button, Grid, Stack, Spinner } from "@chakra-ui/react"
 import axios from "axios"
 import { useEffect, useState } from "react"
 import Product from "./Products"
 
 
+
+
+
 export default function Home() {
 
   const [ProductData, setProductData]=useState([])
+  const [catg, setCatg]=useState("products")
+  const [err, setErr]=useState(false)
+  const [isLoading, setIsLoading]=useState(false)
+
+
+  const getData=(catg)=>{
+    return(axios.get(`http://localhost:${8080}/${catg}`)
+    .then((res)=>
+      res?.data)
+    .catch((err)=>{
+      console.log(err)
+      setErr(true)}))
+    .finally(()=>setIsLoading(false))
+  }
 
   useEffect(() => {
-    axios.get(`http://localhost:${8080}/products`)
-    .then((res)=>setProductData(res?.data))
-    .catch((err)=>console.log(err))  
-    console.log(ProductData)
+    setIsLoading(true)
+    getData(catg)
+    .then((res)=>setProductData(res))
 
-  },[])
+  },[catg])
   
 
 
   return (
-    <>
+    <Stack gap={"50px"} alignItems="center">
+    <Stack spacing={"10px"} direction="row" justifyContent="center">
+      <Button onClick={()=>setCatg("products")}>New</Button>
+      <Button onClick={()=>setCatg("male")}>Men</Button>
+      <Button onClick={()=>setCatg("female")}>Women</Button>
+    </Stack>
 
-    <Button>General Products</Button>
-    <Button>Male Products</Button>
-    <Button>Female Products</Button>
-
-    <Grid templateColumns='repeat(4, 1fr)' gap={"20px"} margin="50px">
+    {isLoading?<Spinner
+  thickness='4px'
+  speed='0.65s'
+  emptyColor='gray.200'
+  color='blue.500'
+  size='xl'
+/>:err?<h1>Something went wrong</h1>:<Grid templateColumns={["repeat(1, 1fr)", "repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)", "repeat(4, 1fr)"]} gap={"20px"} margin="100px">
         {ProductData.map((product)=>(
-        <Product props={product}/>
+        <Product props={product} key={product.id}/>
         ))}
-    </Grid>
-    </>
+    </Grid>}
+    </Stack>
 
   )
 }
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,39 +1,63 @@
 import React from 'react'
 import {Card, CardBody, Image, Stack, Heading, Text, Divider, CardFooter, ButtonGroup, Button} from "@chakra-ui/react"
+import { useToast } from '@chakra-ui/react'
+
+function BookNow() {
+  const toast = useToast()
+  return (
+    <Button
+    variant='solid' colorScheme='blue'
+      onClick={() =>
+        toast({
+          title: 'You Order has Placed Successfully.',
+          description: `Your order id ${Math.floor(Math.random()*10000)}`,
+          status: 'success',
+          duration: 8000,
+          closable: true,
+        })
+      }
+    >
+      Buy Now
+    </Button>
+  )
+}
+
+
+
 function Product({props}) {
-  const {id, brand, description, images, name, price, ratings, strikeOfPrice, type}= props
+  const {id, description, images, name, price, ratings, strikeOfPrice}= props
 
   return (
     <div>
-      <Card maxW='sm' key={id} width={"auto"}>
-  <CardBody>
+      <Card maxW='sm' key={id} width={"auto"} height="760px">
+    <CardBody>
     <Image
       src={images}
       alt={props.name}
       borderRadius='lg'
-      height={"50%"}
+      height={"300px"}
+      margin="auto"
     />
     <Stack mt='6' spacing='3'>
       <Heading size='md'>{name}</Heading>
       <Text>
         {description}
       </Text>
-      <Text color='blue.600' fontSize='xl' as='del'>
+      <Text color='gray.600' fontSize='xl' as='del'>
         ${strikeOfPrice}
       </Text>
       <Text color='blue.600' fontSize='2xl'>
         ${price}
       </Text>
-
+      <Text as="p">Rating : {ratings?.rating}</Text>
     </Stack>
   </CardBody>
   <Divider />
   <CardFooter>
     <ButtonGroup spacing='2'>
-      <Button variant='solid' colorScheme='blue'>
-        Buy now
-      </Button>
-      <Button variant='ghost' colorScheme='blue'>
+     <BookNow />
+      <Button variant='ghost' colorScheme='blue'
+      >
         Add to cart
       </Button>
     </ButtonGroup>
